99e2baf996cdb605fc4a0fb35176e05173d4647d
arjun2020-py <arjunkoodathil@gmail.com>
2024-05-01 12:34:00 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,98 @@
+//this is my first js program
+console.log ("Learn about Java script");
+
+//varibles and data types
+
+// var title = 'arjun';
+// document.write(title);
+// document.write("<br>");
+// document.write(typeof title);
+
+// var number  = 10;
+// document.write(number + '<br>');
+// var number  = 15;
+// document.write(number);
+
+// const pi = 3.14;
+// document.write(pi + "<br>");
+// pi = 3;
+// document.write(pi);
+
+// var a = 30,b=40,sum;
+// sum = a % b;
+// document.write(`diffrence is ` + sum)
+
+// var a = 10;
+// a --;
+// document.write(2 ** 3);
+
+
+//assginment operator 
+//1
+//var x = 10;
+
+// //2
+// var result0 = x += 4;
+
+// //3
+// var result1 = x -= 4;
+
+// //4
+// var result2 = x *= 4;
+
+// //5
+// var result3 = x /=4;
+
+// //6
+// var result4 = x %= 4;
+// document.write(result);
+
+
+// var a = 25;
+// var b =  35;
+// document.write(!(a == 20));
+
+
+// var a = 0;
+
+// if (a > 0) {
+
+//     document.write('number is postive');
+    
+// }else if(a < 0){
+//     document.write('number is negative');
+
+// }else{
+//     document.write('Zero');
+// }
+
+
+// var a = 'G';
+// switch (a) {
+//     case 'A':
+//         document.write('Apple');
+//         break;
+//     case 'B': 
+//          document.write('Bell');
+//          break;
+//     case 'C': 
+//          document.write('Car');
+//          break;
+//     case 'D':
+//          document.write('Dog');
+//          break;
+//     case 'E' : 
+//          document.write('Elephant');
+//          break;
+    
+
+//     default:
+//         document.write('Invaild Character');
+//         break;
+// }
+
+ for (let index = 10; index >= 6; index++) {
+    document.write(index + '<br>');
+    
+ }
+
